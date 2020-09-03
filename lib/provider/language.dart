997e826a66375c49612d894cb6ed7fddd9fb8c00
nattawut <nattawut@mecode.asia>
2020-09-03 16:06:59 +0700

    feat : add language models

---
+++ lib/provider/language.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mechange_app/models/language.dart';
+
+class LanguageProvider with ChangeNotifier{
+  Language language;
+
+  LanguageProvider({@required this.language});
+
+  changeLanguage(Language language){
+    this.language = language;
+  }
+}
