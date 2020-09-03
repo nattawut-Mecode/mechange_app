7ec6c0da8b0d428f2c73d6df4f815039e8324f52
nattawut <nattawut@mecode.asia>
2020-09-03 16:06:42 +0700

    feat : add vairable state for darkmode

--- lib/provider/theme.dart
+++ lib/provider/theme.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 class ThemeProvider with ChangeNotifier{
   Color themePrimary;
+  Brightness brightness;
 
   changePrimaryColor(Color colorPrimary){
     themePrimary = colorPrimary;
+    notifyListeners();
+  }
+
+  switchToDarkMode(bool switchDark){
+    switchDark? brightness = Brightness.dark:brightness = Brightness.light;
     notifyListeners();
   }
 
