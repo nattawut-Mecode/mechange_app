e7e043604d99f26a31c892c522d414215f4c5320
nattawut <nattawut@mecode.asia>
2020-09-03 13:43:33 +0700

    feat : add appbar and bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:mechange_app/provider/bottom_navigation_index.dart';
+import 'package:mechange_app/provider/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+        create: (_) =>
+            ThemeProvider(themePrimary: Color.fromRGBO(5, 105, 107, 1))),
+    ChangeNotifierProvider(create: (_) => BottomNavigationIndexProvider())
+  ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -9,11 +18,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: "Raleway"
-      ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: "Raleway"),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -31,17 +39,48 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  String title = "MeChange";
+
+  BottomNavigationBarItem bottomNavigator(bool active,String activeAsset,String unActiveAsset,String title,ThemeProvider themeProvider){
+    return BottomNavigationBarItem(
+              icon: SvgPicture.asset(active?activeAsset:unActiveAsset),
+              title: Text(title,style: TextStyle(color: active? themeProvider.themePrimary:Colors.black),));
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeProvider>(context);
+    final selectedIndex = Provider.of<BottomNavigationIndexProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Row(
+          children: [
+            SvgPicture.asset(
+              "assets/logos/Logo_White_nobg.svg",
+              width: 30,
+              height: 30,
+            ),
+            SizedBox(width: 15),
+            Text(title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700)),
+          ],
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 7.5),
+            child: IconButton(
+                icon: Image.asset(
+                  "assets/icons/calculator.png",
+                  width: 25,
+                  height: 25,
+                ),
+                onPressed: () {}),
+          )
+        ],
+        backgroundColor: theme.themePrimary,
       ),
       body: Center(
         child: Column(
@@ -57,10 +96,19 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          selectedIndex.changeSelectedIndex(index);
+        },
+        currentIndex: selectedIndex.selectedIndex,
+        items: [
+          bottomNavigator(selectedIndex.selectedIndex == 0,"assets/icons/usd-circle_select.svg","assets/icons/usd-circle.svg","Currency",theme),
+          bottomNavigator(selectedIndex.selectedIndex == 1,"assets/icons/analytics_select.svg","assets/icons/analytics.svg","Trends",theme),
+          bottomNavigator(selectedIndex.selectedIndex == 2,"assets/icons/rss_select.svg","assets/icons/rss.svg","News",theme),
+          bottomNavigator(selectedIndex.selectedIndex == 3,"assets/icons/map-marked__select.svg","assets/icons/map-marked.svg","News",theme),
+          bottomNavigator(selectedIndex.selectedIndex == 4,"assets/icons/cog_select.svg","assets/icons/cog.svg","Settings",theme),
+        ],
       ),
     );
   }
