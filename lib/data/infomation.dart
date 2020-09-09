f094628ad5657450c3798b78c76200c8b41d9dd4
nattawut <nattawut@mecode.asia>
2020-09-09 17:06:00 +0700

    feat : change email contract us

--- lib/data/infomation.dart
+++ lib/data/infomation.dart
@@ -1,5 +1,5 @@
 class Infomation{
-  static String contractEmail = "Nattawut@mecode.asia";
+  static String contractEmail = "hello@mecode.asia";
   static String assetsLogoNoBG = "assets/logos/Logo_White_nobg.svg";
   static String assetsLogoColor = "assets/logos/Logo_Color.svg";
 }
