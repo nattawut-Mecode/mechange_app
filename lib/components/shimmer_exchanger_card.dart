8d24a81679a2a5e47fae513c50f4564a59c8bd2c
nattawut <nattawut@mecode.asia>
2020-09-11 15:46:25 +0700

    feat : darkmode support shimmer

--- lib/components/shimmer_exchanger_card.dart
+++ lib/components/shimmer_exchanger_card.dart
@@ -1,58 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:mechange_app/provider/theme.dart';
+import 'package:provider/provider.dart';
 import 'package:shimmer/shimmer.dart';
 
-class ShimmerExchangerCard extends StatelessWidget {
+class ShimmerExchangerCard extends StatefulWidget {
+
+
+  @override
+  _ShimmerExchangerCardState createState() => _ShimmerExchangerCardState();
+}
+
+class _ShimmerExchangerCardState extends State<ShimmerExchangerCard> {
+  ThemeProvider themeState;
+
+  Shimmer shimerIcons() {
+    return Shimmer.fromColors(
+        child: ClipOval(
+          child: Container(
+            width: 60,
+            height: 60,
+            color: Colors.red,
+          ),
+        ),
+        baseColor: themeState.brightness == Brightness.dark?Colors.grey[800]:Colors.grey[300],
+        highlightColor: themeState.brightness == Brightness.dark?Colors.grey[700]: Colors.white);
+  }
+
+  Shimmer shimerTitle() {
+    return Shimmer.fromColors(
+        baseColor: themeState.brightness == Brightness.dark?Colors.grey[800]:Colors.grey[300],
+        highlightColor: themeState.brightness == Brightness.dark?Colors.grey[700]: Colors.white,
+        child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 150, maxHeight: 70),
+            child: ListTile(
+              title: Container(height: 20, child: Card()),
+              subtitle: Container(height: 20, child: Card()),
+            )));
+  }
+
+  Shimmer shimerExchangeRate() {
+    return Shimmer.fromColors(
+      baseColor: themeState.brightness == Brightness.dark?Colors.grey[800]:Colors.grey[300],
+      highlightColor: themeState.brightness == Brightness.dark?Colors.grey[700]: Colors.white,
+      child: Container(
+        width: 55,
+        height: 20,
+        child: Card(),
+      ),
+    );
+  }
+
+  Shimmer shimmerPercentRate() {
+    return Shimmer.fromColors(
+      baseColor: themeState.brightness == Brightness.dark?Colors.grey[800]:Colors.grey[300],
+        highlightColor: themeState.brightness == Brightness.dark?Colors.grey[700]: Colors.white,
+      child: Container(
+        width: 45,
+        height: 20,
+        child: Card(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    themeState = Provider.of<ThemeProvider>(context);
     return Container(
         margin: EdgeInsets.only(top: 17.5),
         child: Row(
           children: [
             SizedBox(width: 17.5),
-            Shimmer.fromColors(
-                child: ClipOval(
-                  child: Container(
-                    width: 60,
-                    height: 60,
-                    color: Colors.red,
-                  ),
-                ),
-                baseColor: Colors.grey[300],
-                highlightColor: Colors.white),
-                SizedBox(width: 5),
-            Shimmer.fromColors(
-              baseColor: Colors.grey[300],
-              highlightColor: Colors.white,
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxWidth: 150,
-                  maxHeight: 70
-                ),
-                child: ListTile(
-                  title: Container(height: 20,child: Card()),
-                  subtitle: Container(height: 20,child: Card()),
-                ))
-              ),
-              Shimmer.fromColors(
-                baseColor: Colors.grey[300],
-                highlightColor: Colors.white,
-                child: Container(
-                  width: 55,
-                  height: 20,
-                  child: Card(),
-                ),
-              ),
-              SizedBox(width: 17.5),
-              Shimmer.fromColors(
-                baseColor: Colors.grey[300],
-                highlightColor: Colors.white,
-                child: Container(
-                  width: 45,
-                  height: 20,
-                  child: Card(),
-                ),
-              )
-              /*Text("0.2860",style: TextStyle(fontSize: 16,fontFamily: "Lato"),),
+            shimerIcons(),
+            SizedBox(width: 5),
+            shimerTitle(),
+            shimerExchangeRate(),
+            SizedBox(width: 17.5),
+            shimmerPercentRate()
+            /*Text("0.2860",style: TextStyle(fontSize: 16,fontFamily: "Lato"),),
               Text("+0.005",style: TextStyle(fontSize: 12,fontFamily: "Lato"),)*/
           ],
         ));
--- lib/components/shimmer_trends_currency.dart
+++ lib/components/shimmer_trends_currency.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
 import 'package:shimmer/shimmer.dart';
 
-class ShimmerTrendsCurrency extends StatelessWidget {
+import '../provider/theme.dart';
+
+class ShimmerTrendsCurrency extends StatefulWidget {
+  @override
+  _ShimmerTrendsCurrencyState createState() => _ShimmerTrendsCurrencyState();
+}
+
+class _ShimmerTrendsCurrencyState extends State<ShimmerTrendsCurrency> {
+  ThemeProvider themeState;
+
   @override
   Widget build(BuildContext context) {
+    themeState = Provider.of<ThemeProvider>(context);
     return Container(
-      margin: EdgeInsets.only(left: 22.5,right: 22.5,top: 12.5),
+      margin: EdgeInsets.only(left: 22.5, right: 22.5, top: 12.5),
       child: Card(
         color: Colors.transparent,
         elevation: 0,
         child: Shimmer.fromColors(
-          baseColor: Colors.grey[100],
-          highlightColor: Colors.grey[300],
+          baseColor: themeState.brightness == Brightness.dark
+              ? Colors.grey[800]
+              : Colors.grey[300],
+          highlightColor: themeState.brightness == Brightness.dark
+              ? Colors.grey[700]
+              : Colors.white,
           child: Container(
             width: MediaQuery.of(context).size.width,
             height: 135,
